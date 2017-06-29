38a11a87f89203b4e1d69b60c975cb25b0eb99f4
מתן חוג'ה <matanhuja@gmail.com>
2017-06-29 20:49:19 +0300

    fix service

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -7,7 +7,7 @@ export class GameService {
   constructor(private http: Http) { }
 
   getImageList(groupNumber: number) {
-    return this.http.get('http://localhost:3000/api/getListFiles/' + groupNumber).map(res =>
+    return this.http.get('/api/getListFiles/' + groupNumber).map(res =>
       res.json()
     );
   }
