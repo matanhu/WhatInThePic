3a0e06ae2f583b145157a9ae3aee07f5b70e82c5
מתן חוג'ה <matanhuja@gmail.com>
2017-06-30 13:15:01 +0300

    add assets folder

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,7 +15,7 @@ router.get('/getListFiles/:groupNumber', (req, res) => {
   var listImageNames = [];
   try{
     // fs.readdir(path.join(__dirname, '../../dist/assets/images/GroupNumber' + req.params.groupNumber), (err, files) => {
-      fs.readdir(path.join(__dirname), (err, files) => {
+      fs.readdir(path.join(__dirname, dist/assets/images/GroupNumber), (err, files) => {
       if(files) {
         files.forEach(file => {
           console.log(file);
